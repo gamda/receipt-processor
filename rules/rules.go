c36b86151d3df907ae52d637023d62324d3308d6
daniel.garcia.marquez <daniel.garciamarquez@zapier.com>
2023-08-19 12:07:51 -0600

    Added function for multiple of 25 cents

--- rules/rules.go
+++ rules/rules.go
@@ -1,6 +1,11 @@
 package rules
 
-func RoundDollar(total string) int {
+import (
+    "math"
+	"strconv"
+)
+
+func PointsForRoundDollar(total string) int {
     points := 50
     if isRoundDollar(total) {
         return points
@@ -13,3 +18,23 @@ func isRoundDollar(total string) bool {
     cents := total[len(total)-2:]
     return cents == "00"
 }
+
+func PointsForMultipleTwentyFiveCents(total string) int {
+    points := 25
+    if isMultipleTwentyFiveCents(total) {
+        return points
+    }
+    return 0
+}
+
+func isMultipleTwentyFiveCents(total string) bool {
+    // Assumption: all `total` variables have a decimal point and two cents digits
+    centsString := total[len(total)-2:]
+    // Assumption: all totals are valid numbers, cents are valid integers
+    cents, err := strconv.ParseFloat(centsString, 64)
+    if (err == nil) {
+        return math.Mod(cents, 25) == 0
+    }
+    return false
+}
+
--- rules/rules_test.go
+++ rules/rules_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestRoundDollarFalse(t *testing.T) {
     total := "11.23"
-    roundDollar := RoundDollar(total)
+    roundDollar := PointsForRoundDollar(total)
     if roundDollar > 0 {
         t.Fatalf(`11.23 is not round`)
     }
@@ -14,8 +14,28 @@ func TestRoundDollarFalse(t *testing.T) {
 
 func TestRoundDollarTrue(t *testing.T) {
     total := "11.00"
-    roundDollar := RoundDollar(total)
+    roundDollar := PointsForRoundDollar(total)
     if roundDollar == 0 {
         t.Fatalf(`11.00 is round`)
+    }
+}
+
+func TestTwentyFiveFalse(t *testing.T) {
+    test_totals := []string{"11.99", "11.01", "11.24", "11.26", "11.49", "11.51", "11.74", "11.76"}
+    for i, total := range test_totals {
+        roundDollar := PointsForMultipleTwentyFiveCents(total)
+        if roundDollar > 0 {
+            t.Fatalf(`Test %v: %v is not multiple of .25`, i, total)
+        }
+    }
+}
+
+func TestTwentyFiveTrue(t *testing.T) {
+    test_totals := []string{"11.00", "11.25", "11.50", "11.75"}
+    for i, total := range test_totals {
+        roundDollar := PointsForMultipleTwentyFiveCents(total)
+        if roundDollar == 0 {
+            t.Fatalf(`Test %v: %v is a multiple of .25`, i, total)
+        }
     }
 }
