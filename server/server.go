285d50fed4973ca9f23cd95a59767177ebcd0be9
daniel.garcia.marquez <daniel.garciamarquez@zapier.com>
2023-08-19 14:45:03 -0600

    Added endpoint to retrieve a receipt's points

--- server/server.go
+++ server/server.go
@@ -5,11 +5,12 @@ import (
     "github.com/gin-gonic/gin"
     "github.com/google/uuid"
     "ReceiptProcessor/rules"
+    "strconv"
 )
 
 var receiptPoints = map[string]float64{}
 
-func processReceipt(c *gin.Context){
+func processReceipt(c *gin.Context) {
     var newReceipt rules.Receipt
     // Call BindJSON to bind the received JSON to newReceipt.
     if err := c.BindJSON(&newReceipt); err != nil {
@@ -26,9 +27,18 @@ func processReceipt(c *gin.Context){
     })
 }
 
+func getReceiptPoints(c *gin.Context) {
+    id := c.Param("id")
+    points := receiptPoints[id]
+    c.IndentedJSON(http.StatusOK, map[string]string{
+        "points": strconv.FormatFloat(points, 'f', -1, 64),
+    })
+}
+
 func main() {
     router := gin.Default()
     router.POST("/receipts/process", processReceipt)
+    router.GET("/receipts/:id/points", getReceiptPoints)
 
     router.Run("localhost:8080")
 }
