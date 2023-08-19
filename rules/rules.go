d3ab8f457aad97f0bea490b86b070f0de7436b28
daniel.garcia.marquez <daniel.garciamarquez@zapier.com>
2023-08-19 11:47:03 -0600

    Function implemented to return amount of points

--- rules/rules.go
+++ rules/rules.go
@@ -1,5 +1,15 @@
 package rules
 
-func RoundDollar(total string) bool {
-    return false
+func RoundDollar(total string) int {
+    points := 50
+    if isRoundDollar(total) {
+        return points
+    }
+    return 0
+}
+
+func isRoundDollar(total string) bool {
+    // Assumption: all `total` variables have a decimal point and two cents digits
+    cents := total[len(total)-2:]
+    return cents == "00"
 }
--- rules/rules_test.go
+++ rules/rules_test.go
@@ -7,15 +7,15 @@ import (
 func TestRoundDollarFalse(t *testing.T) {
     total := "11.23"
     roundDollar := RoundDollar(total)
-    if roundDollar {
+    if roundDollar > 0 {
         t.Fatalf(`11.23 is not round`)
     }
 }
 
 func TestRoundDollarTrue(t *testing.T) {
-    total := "11.23"
+    total := "11.00"
     roundDollar := RoundDollar(total)
-    if !roundDollar {
+    if roundDollar == 0 {
         t.Fatalf(`11.00 is round`)
     }
 }
